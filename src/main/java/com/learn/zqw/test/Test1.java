09a7716096dcbb6cc3a61718f679d9730332127f
zhuquanwen <5211314a>
2020-03-10 21:04:11 +0800

    first commit

--- src/main/java/com/learn/zqw/test/Test1.java
+++ src/main/java/com/learn/zqw/test/Test1.java
@@ -1,5 +1,7 @@
 package com.learn.zqw.test;
 
+import com.learn.zqw.domain.User;
+import com.learn.zqw.mapper.IUserMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -7,6 +9,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * //TODO
@@ -26,7 +29,9 @@ public class Test1 {
     //3.使用工厂生产SqlSession对象
     SqlSession session = factory.openSession ();
     //4.使用SqlSession创建Mapper接口的代理对象
-//    IUserMapper userMapper = session.getMapper(IUserMapper.class);
+    IUserMapper userMapper = session.getMapper(IUserMapper.class);
+    List<User> all = userMapper.findAll();
+    all.forEach(System.out::println);
     session.close();
     in.close();
   }
